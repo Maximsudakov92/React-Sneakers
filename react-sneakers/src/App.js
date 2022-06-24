6eb65e3a58f5a33e3c22b69a68d7ee7883cab540
Maxim Sudakov <grax922@gmail.com>
2022-06-25 00:20:07 +0300

    feat: created header

--- react-sneakers/src/App.js
+++ react-sneakers/src/App.js
@@ -3,9 +3,27 @@
 
 function App() {
   return (
-    <div className="App">
-      <h1>РЕАКТ</h1>
-      <p>новый курс</p>
+    <div className="app">
+      <div className="wrapper">
+        <header>
+          <div className="headerLeft">
+            <img width={40} height={40} src="/img/logo.png" alt="logo" />
+            <div className="headerInfo">
+              <h3>React Sneakers</h3>
+              <p>Магазин лучших кроссовок</p>
+            </div>
+          </div>
+          <ul className="headerRight">
+            <li>
+              <img width={18} height={18} src="/img/cart.svg" alt="cart" />
+              <span>1205 руб.</span>
+            </li>
+            <li><img width={21} height={19} src="/img/favorite.svg" alt="favorite"/></li>
+            <li><img width={20} height={20} src="/img/user.svg" alt="user"/></li>
+          </ul> 
+        </header>
+        
+      </div>
     </div>
   );
 }
